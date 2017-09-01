c48ed48fdb34557d0c5e63cc808b9010d05156cb
Phạm Đặng Hoàng Giang <phamdanghoanggiang@gmail.com>
2017-09-01 18:01:25 +0700

    Egyptian Fraction

---
+++ Greedy/ActivitySelection/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActivitySelection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ActivitySelection asl = new ActivitySelection(new []{ 1, 3, 0, 5, 8, 5 }, new []{ 2, 4, 6, 7, 9, 9 });
+            asl.Compute();
+            asl.Compute2();
+            ActivitySelection asl2 = new ActivitySelection(new []{ 10, 12, 20 }, new []{ 20, 25, 30 });
+            asl2.Compute();
+            asl2.Compute();
+            Console.ReadKey();
+        }
+
+        class ActivitySelection
+        {
+            private readonly int[] _start;
+            private readonly int[] _finish;
+
+            public ActivitySelection(int[] start, int[] finish)
+            {
+                _start = start;
+                _finish = finish;
+            }
+
+            public void Compute()
+            {
+                var sorted = _finish.Select((value, index) => new
+                    {
+                        Start = _start[index],
+                        Finish = value,
+                        Index = index
+                    })
+                    .OrderBy(item => item.Finish)
+                    .ToList();
+                var previous = sorted.First().Index;
+                Console.Write(previous);
+
+                for (var i = 1; i < sorted.Count; i++)
+                {
+                    if (sorted[i].Start >= sorted[previous].Finish)
+                    {
+                        Console.Write(i);
+                        previous = i;
+                    }
+                }
+                Console.WriteLine();
+            }
+
+            public void Compute2()
+            {
+                var previous = 0;
+                Console.Write(previous);
+                for (var i = 1; i < _finish.Length; i++)
+                {
+                    if (_start[i] >= _finish[previous])
+                    {
+                        Console.Write(i);
+                        previous = i;
+                    }
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Greedy/EgyptianFraction/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EgyptianFraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Test(2, 3);
+            Test(6, 14);
+            Test(12, 13);
+            Console.ReadKey();
+        }
+
+        private static void Test(int numerator, int denominator)
+        {
+            EgyptianFraction ef = new EgyptianFraction(numerator, denominator);
+            Console.WriteLine(ef.GetFraction + "=" + string.Join(" + ", ef.GetUnitFractions()));
+        }
+
+        public class Fraction
+        {
+            public int Numerator { get; set; }
+            public int Denominator { get; set; }
+
+            public static Fraction operator -(Fraction f1, Fraction f2)
+            {
+                return new Fraction
+                {
+                    Numerator = f1.Numerator * f2.Denominator - f1.Denominator * f2.Numerator,
+                    Denominator = f1.Denominator * f2.Denominator
+                };
+            }
+
+            public override string ToString()
+            {
+                return string.Format("{0}/{1}", Numerator, Denominator);
+            }
+        }
+
+        public class EgyptianFraction
+        {
+            private readonly int _numerator;
+            private readonly int _denominator;
+
+            public Fraction GetFraction
+            {
+                get
+                {
+                    return new Fraction
+                    {
+                        Numerator = _numerator,
+                        Denominator = _denominator
+                    };
+                }
+            }
+
+            public EgyptianFraction(int numerator, int denominator)
+            {
+                _numerator = numerator;
+                _denominator = denominator;
+            }
+
+            public ICollection<Fraction> GetUnitFractions()
+            {
+                var result = new List<Fraction>();
+                var origin = new Fraction
+                {
+                    Numerator = _numerator,
+                    Denominator = _denominator
+                };
+                while (origin.Numerator != 0)
+                {
+                    var unit = new Fraction
+                    {
+                        Numerator = 1,
+                        Denominator = (int) Math.Ceiling(origin.Denominator / (double) origin.Numerator)
+                    };
+                    result.Add(unit);
+                    origin -= unit;
+                }
+                return result;
+            }
+        }
+    }
+}
